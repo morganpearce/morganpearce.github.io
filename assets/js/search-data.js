9e73b82d9306fb2b0aab12a6e6fb7a1c403299e7
morganpearce <91696600+morganpearce@users.noreply.github.com>
2025-07-04 22:18:00 +0000

    Deploying to gh-pages from @ morganpearce/morganpearce.github.io@8f224698be31b9dda8c40f5961b885fa00cd007e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -447,36 +447,11 @@ ninja.data = [{
           description: "A personal project in which I am using Textual Communities to transcribe and edit the 3 manuscript witnesses and the 1 early print edition of Chaucer&#39;s poem.",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
             },},{id: "projects-ttrpg",
           title: 'TTRPG',
           description: "a project with a background image",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
-          title: 'project 9',
-          description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
             },},{
         id: 'social-email',
         title: 'email',
